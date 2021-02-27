1e06495616111448f668bfe345125a6a75673328
Sam van der Kris <samvdkris@disroot.org>
2021-02-27 11:46:23 +0100

    Add hello world index page

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Spring demo
 
 Hello yes this is a test repo
+
+This application is deployed using Heroku at https://spring-test-sam.herokuapp.com/
--- src/main/java/xyz/samvanderkris/demo/DemoApplication.java
+++ src/main/java/xyz/samvanderkris/demo/DemoApplication.java
@@ -2,9 +2,19 @@ package xyz.samvanderkris.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello world!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
